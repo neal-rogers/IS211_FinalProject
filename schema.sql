e2803becad542ddaaf2c8b09eadd11aaa8d4a1b9
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-05-21 22:22:41 -0400

    Program doesn't have edit capability.

--- schema.sql
+++ schema.sql
@@ -1,27 +1,6 @@
-drop table if exists Students;
-drop table if exists Quizzes;
-drop table if exists Results;
-
-CREATE TABLE Students (
-    id integer PRIMARY KEY autoincrement,
-    firstname text not null,
-    lastname text not null
-    );
-
-CREATE TABLE Quizzes (
-    id integer PRIMARY KEY autoincrement,
-    subject text not null,
-    questions integer not null,
-    qdate date not null
-    );
-
-CREATE TABLE Results (
-    id integer PRIMARY KEY autoincrement,
-    quizid integer,
-    studentid int,
-    grade float
-    );
-
-INSERT INTO Students VALUES(Null, 'John', 'Smith');
-INSERT INTO Quizzes VALUES(Null,'Python Basics', 5, '2/05/2015');
-INSERT INTO Results VALUES(Null,(select ID from Quizzes where subject = 'Python Basics'),(select ID from Students where firstName = 'John' and lastName = 'Smith'),85;
+drop table if exists entries;
+create table Entries (
+  id integer primary key autoincrement,
+  title text not null,
+  text text not null
+);
